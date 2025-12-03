b1a8ce50380fe7583fe193d702047b776ce4c014
Dor Serero <dor.serero@gmail.com>
2025-12-03 15:23:49 +0000

    feat: handle unshare
    Signed-off-by: Dor Serero <dor.serero@gmail.com>

--- gadgets/cap-restrict/program.bpf.h
+++ gadgets/cap-restrict/program.bpf.h
@@ -11,8 +11,28 @@
 #define CAP_SYS_MODULE 16
 #endif
 
+#ifndef CAP_SYS_ADMIN
+#define CAP_SYS_ADMIN 21
+#endif
+
+#define CLONE_NEWNS 0x00020000
+#define CLONE_NEWCGROUP 0x02000000
+#define CLONE_NEWUTS 0x04000000
+#define CLONE_NEWIPC 0x08000000
+#define CLONE_NEWUSER 0x10000000
+#define CLONE_NEWPID 0x20000000
+#define CLONE_NEWNET 0x40000000
+
 struct event {
   gadget_timestamp timestamp_raw;
   struct gadget_process process;
   int cap;
+  unsigned long flags;
 };
+
+struct {
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __uint(max_entries, 1024);
+  __type(key, u64);
+  __type(value, unsigned long);
+} catch_at_cap SEC(".maps");
