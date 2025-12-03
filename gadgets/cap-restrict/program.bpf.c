023ff83923723398f2b4c7ebff8251c2ea75ec80
Dor Serero <dor.serero@gmail.com>
2025-12-03 15:23:49 +0000

    feat: handle unshare
    Signed-off-by: Dor Serero <dor.serero@gmail.com>

--- gadgets/cap-restrict/program.bpf.c
+++ gadgets/cap-restrict/program.bpf.c
@@ -16,6 +16,28 @@ GADGET_TRACER_MAP(events, 1024 * 256);
 
 GADGET_TRACER(cap_restrict, events, event);
 
+static __always_inline int check_unshare_flags(unsigned long flags) {
+  unsigned long ns_flags = CLONE_NEWNS | CLONE_NEWCGROUP | CLONE_NEWUTS |
+                           CLONE_NEWIPC | CLONE_NEWUSER | CLONE_NEWPID |
+                           CLONE_NEWNET;
+  return (flags & ns_flags);
+}
+
+SEC("tracepoint/syscalls/sys_enter_unshare")
+int ig_unshare_enter(struct syscall_trace_enter *ctx) {
+  if (gadget_should_discard_data_current())
+    return 0;
+
+  unsigned long flags = ctx->args[0];
+  if (!check_unshare_flags(flags))
+    return 0;
+
+  u64 pid = bpf_get_current_pid_tgid();
+  bpf_map_update_elem(&catch_at_cap, &pid, &flags, BPF_ANY);
+
+  return 0;
+}
+
 SEC("lsm/capable")
 int BPF_PROG(micromize_capable, const struct cred *cred,
              struct user_namespace *ns, int cap, unsigned int opts) {
@@ -29,6 +51,21 @@ int BPF_PROG(micromize_capable, const struct cred *cred,
   event = gadget_reserve_buf(&events, sizeof(*event));
   if (!event)
     return 0;
+
+  if (cap == CAP_SYS_ADMIN) {
+    u64 pid = bpf_get_current_pid_tgid();
+    unsigned long *flags;
+
+    flags = bpf_map_lookup_elem(&catch_at_cap, &pid);
+    bpf_map_delete_elem(&catch_at_cap, &pid);
+
+    if (!flags) {
+      gadget_discard_buf(event);
+      return 0;
+    }
+
+    event->flags = *flags;
+  }
 
   gadget_process_populate(&event->process);
   event->timestamp_raw = bpf_ktime_get_boot_ns();
