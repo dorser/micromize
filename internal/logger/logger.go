0b73b96ce17c477a9f093d3d9f0cfd40825b9a07
Dor Serero <dor.serero@gmail.com>
2026-01-05 14:18:11 +0000

    fix: add logrus to enable ig debugging
    Signed-off-by: Dor Serero <dor.serero@gmail.com>

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,19 +4,34 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
-// Setup configures the default slog logger. The verbose flag takes precedence; if true, debug level is used. Otherwise, LOG_LEVEL environment variable is checked, defaulting to INFO if not set or invalid.
+// Setup configures the default slog logger and the global logrus logger. The verbose flag takes precedence; if true, debug level is used for both. Otherwise, the LOG_LEVEL environment variable is checked to set the level for both, defaulting to INFO if not set or invalid.
 func Setup(verbose bool) {
 	level := slog.LevelInfo
+	logrusLevel := logrus.InfoLevel
+
 	if verbose {
 		level = slog.LevelDebug
+		logrusLevel = logrus.DebugLevel
 	} else {
 		if envLevel := os.Getenv("LOG_LEVEL"); envLevel != "" {
+			var slogOk, logrusOk bool
+
 			var l slog.Level
 			if err := l.UnmarshalText([]byte(envLevel)); err == nil {
 				level = l
-			} else {
+				slogOk = true
+			}
+
+			if parsedLevel, err := logrus.ParseLevel(envLevel); err == nil {
+				logrusLevel = parsedLevel
+				logrusOk = true
+			}
+
+			if !slogOk && !logrusOk {
 				fmt.Fprintf(os.Stderr, "Invalid LOG_LEVEL %q, defaulting to INFO\n", envLevel)
 			}
 		}
@@ -27,4 +42,6 @@ func Setup(verbose bool) {
 	}
 	logger := slog.New(slog.NewTextHandler(os.Stderr, opts))
 	slog.SetDefault(logger)
+
+	logrus.SetLevel(logrusLevel)
 }
